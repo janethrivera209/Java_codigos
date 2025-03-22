3c772b914ed2df3a25e7d086efe58c7a47a7d5af
janethrivera209 <riverarobledojaneth@gmail.com>
2025-03-21 20:08:39 -0600

    Add files via upload

---
+++ numeros.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+
+public class ejercicio1 {
+    public static void main (String [] args){
+        int valor1;
+        valor1=Integer.parseInt(JOptionPane.showInputDialog(null,"ingresa el valor 1"));
+        int valor2;
+        valor2=Integer.parseInt(JOptionPane.showInputDialog(null,"ingresa el valor 2"));
+   if (valor1>valor2){
+        System.out.print("El numero mayor es :" +valor1);
+    }   
+   else{
+       if (valor1<valor2) {
+           System.out.print("El numero mayor es:" +valor2);
+       }
+       else{
+           if (valor1==valor2) {
+               System.out.print("Ambos valores son iguales, por favor intentalo de nuevo");
+           }
+       }
+   }
+    }
+    
+}
