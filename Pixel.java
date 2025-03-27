8c214686ecd7df39ca1a58834bd199301ee2633f
janethrivera209 <riverarobledojaneth@gmail.com>
2025-03-26 20:02:11 -0600

    Add files via upload

---
+++ Pixel.java
@@ -0,0 +1,105 @@
+import javax.swing.JOptionPane;//importando paquete
+public class Pixel {//creando clase
+    public static void main (String [] args){//metodo principal
+        float x;
+        x=1;
+        for(x=1;x<=1;x++){
+            System.out.print("\n               ");
+            for(x=1;x<=1;x++){
+                System.out.print("*");
+            }
+            
+         }
+        for(x=1;x<=1;x++){
+            System.out.print("\n              ");
+            for(x=1;x<=3;x++){
+                System.out.print("*");
+            }
+        }
+        for(x=1;x<=1;x++){
+            System.out.print("\n             ");
+            for(x=1;x<=5;x++){
+                System.out.print("*");
+            }
+        }
+        for(x=1;x<=1;x++){
+            System.out.print("\n            ");
+            for(x=1;x<=7;x++){
+                System.out.print("*");
+            }
+        }
+        for(x=1;x<=1;x++){
+            System.out.print("\n        ");
+            for(x=1;x<=15;x++){
+                System.out.print("*");
+            }
+        }
+         for(x=1;x<=1;x++){
+            System.out.print("\n         ");
+            for(x=1;x<=13;x++){
+                System.out.print("*");
+            }
+         }
+         for(x=1;x<=1;x++){
+            System.out.print("\n          ");
+            for(x=1;x<=11;x++){
+                System.out.print("*");
+            }
+         }
+         for(x=1;x<=1;x++){
+            System.out.print("\n           ");
+            for(x=1;x<=9;x++){
+                System.out.print("*");
+            }
+         }
+         for(x=1;x<=1;x++){
+            System.out.print("\n            ");
+            for(x=1;x<=7;x++){
+                System.out.print("*");
+            }
+    }
+         for(x=1;x<=1;x++){
+            System.out.print("\n             ");
+            for(x=1;x<=5;x++){
+                System.out.print("*");
+            }
+         }
+         for(x=1;x<=1;x++){
+            System.out.print("\n              ");
+            for(x=1;x<=3;x++){
+                System.out.print("*");
+            }
+         }
+         for(x=1;x<=1;x++){
+            System.out.print("\n               ");
+            for(x=1;x<=1;x++){
+                System.out.print("*");
+            }
+         }
+         for(x=1;x<=1;x++){
+            System.out.print("\n        ");
+            for(x=1;x<=15;x++){
+                System.out.print("*");
+            }
+         }
+         for(x=1;x<=1;x++){
+            System.out.print("\n       ");
+            for(x=1;x<=17;x++){
+                System.out.print("*");
+            }
+         }
+         for(x=1;x<=1;x++){
+            System.out.print("\n      ");
+            for(x=1;x<=19;x++){
+                System.out.print("*");
+            }
+         }
+         for(x=1;x<=1;x++){
+            System.out.print("\n     ");
+            for(x=1;x<=21;x++){
+                System.out.print("*");
+            }
+         }
+         
+    }
+}
